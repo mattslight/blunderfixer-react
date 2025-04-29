f314bd07e79556c04affda89f2556b2c27902744
Matthew Slight <matthew@slight.me>
2025-04-29 09:50:54 +0400

    handle sidebar toggle state in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import AppRoutes from "./routes";
+import { useState } from "react";
 
 export default function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
-  <>
-    <Navbar />
-    <Sidebar />
-    <main class="p-4 md:ml-64 h-auto pt-20 w-full">
-      <AppRoutes />
-    </main>
-  </>
+    <>
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar isSidebarOpen={isSidebarOpen} />
+      <main class="h-auto w-full p-4 pt-20 md:ml-64">
+        <AppRoutes />
+      </main>
+    </>
   );
 }
