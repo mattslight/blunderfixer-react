48e2774a92ade56073f16f3591d7cf5761e99185
Matthew Slight <matthew@slight.me>
2025-04-30 14:55:15 +0400

    update game history page

--- src/pages/GameHistory.jsx
+++ src/pages/GameHistory.jsx
@@ -1,8 +1,10 @@
+import GameLoader from './AnalysePosition/GameLoader';
+
 export default function GameHistory() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Game History</h1>
-      <p className="text-lg">This is the Games History page.</p>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="mt-20 mb-4 text-4xl font-bold">Game History</h1>
+      <GameLoader />
     </div>
   );
 }
