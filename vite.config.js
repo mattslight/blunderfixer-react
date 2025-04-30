6f6d5240c3e4c63dac6b5c8356db8123123bbd28
Matthew Slight <matthew@slight.me>
2025-04-30 22:18:19 +0400

    set alias for src/ path so we can include with @/

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 // https://vite.dev/config/
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     host: true, // same as '0.0.0.0'
     port: 5173,
@@ -22,6 +23,7 @@ export default defineConfig({
         __dirname,
         'tailwindcss-version-shim.js'
       ),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
