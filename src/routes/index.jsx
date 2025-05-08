7adabea580da7daf8991fd3b5db8a8ec1efcb07e
Matthew Slight <matthew@slight.me>
2025-05-08 15:50:58 +0400

    refactor: remove unused GameSummary and PasteForm components

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
 import AnalyseGame from '../pages/analyse';
-import DebugGameStore from '../pages/analyse/DebugGameStore';
 import GameHistory from '../pages/games';
 import Help from '../pages/Help';
 import PreSignupHome from '../pages/home/PreSignupHome';
@@ -16,7 +15,6 @@ export default function AppRoutes() {
     <Routes>
       <Route path="/" element={<PreSignupHome />} />
       <Route path="/insights" element={<Insights />} />
-      <Route path="/analyse/debug" element={<DebugGameStore />} />
       <Route path="/analyse" element={<AnalyseGame />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/games" element={<GameHistory />} />
