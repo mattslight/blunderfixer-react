bbb34d53c80247c0cd80cdaaf6c4057cc8407484
Matthew Slight <matthew@slight.me>
2025-05-10 16:13:30 +0400

    feat: implement PreSignupGuard and ProtectedRoute for enhanced user authentication

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Training from '@/pages/_placeholders/Training';
+import ReportPage from '@/pages/report';
+import Settings from '@/pages/settings';
 import AnalyseGame from '../pages/analyse';
 import GameHistory from '../pages/games';
 import Help from '../pages/Help';
@@ -7,22 +10,84 @@ import PreSignupHome from '../pages/home/PreSignupHome';
 import Insights from '../pages/Insights';
 import NotFound from '../pages/NotFound';
 import Profile from '../pages/Profile';
-import Settings from '@/pages/settings';
-import Training from '@/pages/_placeholders/Training';
-import ReportPage from '@/pages/report';
+import PreSignupGuard from './PreSignupGuard';
+import ProtectedRoute from './ProtectedRoute';
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<PreSignupHome />} />
-      <Route path="/insights" element={<Insights />} />
-      <Route path="/analyse" element={<AnalyseGame />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/games" element={<GameHistory />} />
-      <Route path="/train" element={<Training />} />
-      <Route path="/help" element={<Help />} />
-      <Route path="/report/:analysisId" element={<ReportPage />} />
-      <Route path="/settings" element={<Settings />} />
+      <Route
+        path="/"
+        element={
+          <PreSignupGuard>
+            <PreSignupHome />
+          </PreSignupGuard>
+        }
+      />
+      <Route
+        path="/insights"
+        element={
+          <ProtectedRoute>
+            <Insights />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/analyse"
+        element={
+          <ProtectedRoute>
+            <AnalyseGame />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/games"
+        element={
+          <ProtectedRoute>
+            <GameHistory />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/train"
+        element={
+          <ProtectedRoute>
+            <Training />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/help"
+        element={
+          <ProtectedRoute>
+            <Help />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/report/:analysisId"
+        element={
+          <ProtectedRoute>
+            <ReportPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <ProtectedRoute>
+            <Settings />
+          </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
