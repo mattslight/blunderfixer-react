f96be01d6ce7fdd0a3b8783204952b123fe2d556
Matthew Slight <matthew@slight.me>
2025-05-11 11:53:52 +0400

    refactor: reorder imports and add flowbite path in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
 
-import { defineConfig } from 'tailwindcss';
-import flowbite from 'flowbite/plugin';
 import typography from '@tailwindcss/typography';
+import flowbite from 'flowbite/plugin';
+import { defineConfig } from 'tailwindcss';
 
 /** @type {import('tailwindcss').Config} */
 export default defineConfig({
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
   ],
   theme: { extend: {} },
   plugins: [flowbite, typography],
