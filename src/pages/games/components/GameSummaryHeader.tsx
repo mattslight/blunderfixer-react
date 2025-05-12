9e42ba49883a5bba94fe06c2f97946051e15bee0
Matthew Slight <matthew@slight.me>
2025-05-13 00:42:19 +0400

    refactor: simplify GameSummaryHeader component by removing unused date formatting and enhancing layout

--- src/pages/games/components/GameSummaryHeader.tsx
+++ src/pages/games/components/GameSummaryHeader.tsx
@@ -1,29 +1,17 @@
-import { formatDistanceToNow } from 'date-fns';
-
 export default function GameSummaryHeader({ game }) {
-  // assume game.meta.endTime is seconds since epoch
-  const dateObj = new Date(game.meta.endTime * 1000);
-  const dateStr = formatDistanceToNow(dateObj, { addSuffix: true }).replace(
-    /^about\s*/,
-    ''
-  );
   return (
-    <header className="mb-4 flex items-center justify-between">
-      <div>
-        <h3 className="text-xl font-medium">
-          {game.meta.players.white.player.username}{' '}
-          <span className="text-2xl text-white">♞</span>{' '}
-          <span className="px-2 text-lg text-gray-400">vs.</span>{' '}
-          {game.meta.players.black.player.username}{' '}
-          <span className="text-2xl text-black [text-shadow:-0.5px_-0.5px_0_#4F46E5,0.5px_-0.5px_0_#4F46E5,-0.5px_0.5px_0_#4F46E5,0.5px_0.5px_0_#4F46E5]">
-            ♞
-          </span>
-        </h3>
-        <p className="text-sm text-gray-400">
-          {dateStr} • {game.meta.timeControl}+{game.meta.increment}s •{' '}
-          {game.meta.timeClass}
-        </p>
-      </div>
+    <header className="mb-8 flex items-center justify-between">
+      <h3 className="align-middle text-xl font-medium">
+        <span className="text-3xl text-white">♞</span>{' '}
+        {game.meta.players.white.player.username}{' '}
+        {game.meta.players.white.player.rating}
+      </h3>
+      <h3 className="text-xl font-medium text-white">
+        <span className="text-3xl text-black [text-shadow:-0.5px_-0.5px_0_#4F46E5,0.5px_-0.5px_0_#4F46E5,-0.5px_0.5px_0_#4F46E5,0.5px_0.5px_0_#4F46E5]">
+          ♞
+        </span>{' '}
+        {game.meta.players.black.player.username}{' '}
+      </h3>
     </header>
   );
 }
