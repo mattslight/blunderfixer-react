26395b248117c5d3cb3c5d562288a6369c60c2e9
Matthew Slight <matthew@slight.me>
2025-05-13 11:21:39 +0400

    refactor: enhance GameSummaryHeader layout and improve date display

--- src/pages/games/components/GameSummaryHeader.tsx
+++ src/pages/games/components/GameSummaryHeader.tsx
@@ -1,17 +1,30 @@
+import { formatDistanceToNow } from 'date-fns';
+
 export default function GameSummaryHeader({ game }) {
+  // assume game.meta.endTime is seconds since epoch
+  const dateObj = new Date(game.meta.endTime * 1000);
+  const dateStr = formatDistanceToNow(dateObj, { addSuffix: true }).replace(
+    /^about\s*/,
+    ''
+  );
   return (
-    <header className="mb-8 flex items-center justify-between">
-      <h3 className="align-middle text-xl font-medium">
-        <span className="text-3xl text-white">♞</span>{' '}
-        {game.meta.players.white.player.username}{' '}
-        {game.meta.players.white.player.rating}
-      </h3>
-      <h3 className="text-xl font-medium text-white">
-        <span className="text-3xl text-black [text-shadow:-0.5px_-0.5px_0_#4F46E5,0.5px_-0.5px_0_#4F46E5,-0.5px_0.5px_0_#4F46E5,0.5px_0.5px_0_#4F46E5]">
-          ♞
-        </span>{' '}
-        {game.meta.players.black.player.username}{' '}
-      </h3>
+    <header className="mb-4 flex items-center justify-between">
+      <div>
+        <h3 className="text-xl font-medium">
+          {game.meta.players.white.player.username}{' '}
+          <span className="text-2xl text-white">♞</span>{' '}
+          <span className="px-4 text-lg text-gray-600">vs</span>{' '}
+          {game.meta.players.black.player.username}{' '}
+          <span className="text-2xl text-black [text-shadow:-0.5px_-0.5px_0_#4F46E5,0.5px_-0.5px_0_#4F46E5,-0.5px_0.5px_0_#4F46E5,0.5px_0.5px_0_#4F46E5]">
+            ♞
+          </span>
+        </h3>
+        <p className="text-sm font-medium text-gray-600">
+          {dateStr} <span className="text-gray-700">•</span>{' '}
+          {game.meta.timeControl / 60}m+{game.meta.increment}s{' '}
+          <span className="text-gray-700">•</span> {game.meta.timeClass}
+        </p>
+      </div>
     </header>
   );
 }
