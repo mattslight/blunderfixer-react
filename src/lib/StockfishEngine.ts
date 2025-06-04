841e707d12cc91192649ef34f34aea07394a1f11
Matthew Slight <matthew@slight.me>
2025-06-05 02:41:08 +0400

    Disable debug mode in StockfishEngine

--- src/lib/StockfishEngine.ts
+++ src/lib/StockfishEngine.ts
@@ -1,7 +1,7 @@
 // src/lib/StockfishEngine.ts
 import { Subject, Subscription } from 'rxjs';
 
-const DEBUG = true;
+const DEBUG = false;
 
 /**
  * A lightweight wrapper around a Stockfish WebAssembly worker.
