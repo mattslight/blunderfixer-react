9e7bc346fd74080d9d6e5dece18b418a2474bb24
Matthew Slight <matthew@slight.me>
2025-06-07 15:08:03 +0400

    feat: implement BackgroundPatternProvider for managing background pattern state

---
+++ src/contexts/BackgroundPatternProvider.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+import { BackgroundPatternContext } from './BackgroundPatternContext';
+
+import { BackgroundPattern } from '@/constants/background';
+
+const PREFIX = 'bf:params:backgroundPattern';
+
+export function BackgroundPatternProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [pattern, setPatternState] = useState<BackgroundPattern>('off');
+
+  useEffect(() => {
+    const stored = localStorage.getItem(PREFIX);
+    if (stored) setPatternState(JSON.parse(stored));
+  }, []);
+
+  const setPattern = (val: BackgroundPattern) => {
+    setPatternState(val);
+    localStorage.setItem(PREFIX, JSON.stringify(val));
+  };
+
+  return (
+    <BackgroundPatternContext.Provider value={{ pattern, setPattern }}>
+      {children}
+    </BackgroundPatternContext.Provider>
+  );
+}
