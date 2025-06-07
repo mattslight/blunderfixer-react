4b7944b3cd4a00dbc84dd4c060eb01727a353542
Matthew Slight <matthew@slight.me>
2025-06-07 15:06:54 +0400

    fix: remove duplicate import of BackgroundLayer and enhance main element styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+// src/App.jsx
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
+import BackgroundLayer from './components/BackgroundLayer';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import AppRoutes from './routes';
-import BackgroundLayer from './components/BackgroundLayer';
 
 import { useProfile } from '@/hooks/useProfile';
 
@@ -35,7 +36,9 @@ export default function App() {
       <BackgroundLayer />
       <Navbar toggleSidebar={toggleSidebar} />
       <Sidebar isSidebarOpen={isSidebarOpen} closeSidebar={closeSidebar} />
-      <main className={`min-h-screen w-full ${loggedIn && 'pt-8 2xl:pl-32'}`}>
+      <main
+        className={`z-10 min-h-screen w-full ${loggedIn && 'pt-8 2xl:pl-32'}`}
+      >
         <AppRoutes />
       </main>
     </>
