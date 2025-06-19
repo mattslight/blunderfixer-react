a5d29e04e94aaad9732714ef78579ca194aeb0ff
Matthew Slight <matthew@slight.me>
2025-06-19 23:19:10 +0400

    fix: update label for insights link in Sidebar and remove redundant drillsNav object for cleaner code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ export default function Sidebar({ isSidebarOpen, closeSidebar }) {
   const mainNav = [
     {
       to: '/insights',
-      label: 'Insights',
+      label: 'Home',
       Icon: ({ className }) => (
         <svg
           aria-hidden="true"
@@ -24,6 +24,25 @@ export default function Sidebar({ isSidebarOpen, closeSidebar }) {
         >
           <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
           <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+        </svg>
+      ),
+    },
+    {
+      to: '/drills',
+      label: 'Drills',
+      Icon: ({ className }) => (
+        <svg
+          aria-hidden="true"
+          className={className}
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+            clipRule="evenodd"
+          ></path>
         </svg>
       ),
     },
@@ -76,26 +95,6 @@ export default function Sidebar({ isSidebarOpen, closeSidebar }) {
     //   ),
     // },
   ];
-
-  const drillsNav = {
-    to: '/drills',
-    label: 'Drills',
-    Icon: ({ className }) => (
-      <svg
-        aria-hidden="true"
-        className={className}
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-          clipRule="evenodd"
-        ></path>
-      </svg>
-    ),
-  };
 
   const endgamesNav = {
     to: '/endgames',
@@ -178,13 +177,6 @@ export default function Sidebar({ isSidebarOpen, closeSidebar }) {
               <LinkItem to={to} label={label} Icon={Icon} />
             </li>
           ))}
-          <li>
-            <LinkItem
-              to={drillsNav.to}
-              label={drillsNav.label}
-              Icon={drillsNav.Icon}
-            />
-          </li>
         </ul>
 
         <ul className="mt-5 space-y-2 border-t border-stone-200 pt-5 dark:border-stone-700">
